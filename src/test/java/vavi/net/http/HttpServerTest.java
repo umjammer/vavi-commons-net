0757827bafd7fa443512a5818c4ff50740753693
umjammer <umjammer@gmail.com>
2020-05-06 03:45:24 +0900

    💄

--- src/test/java/vavi/net/http/HttpServerTest.java
+++ src/test/java/vavi/net/http/HttpServerTest.java
@@ -14,6 +14,7 @@ import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertThrows;
 
+
 /**
  * HttpServerTest.
  *
@@ -46,7 +47,6 @@ class HttpServerTest {
         conn.getContent();
         server.stop();
     }
-
 }
 
 /* */
