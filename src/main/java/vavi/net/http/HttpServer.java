21d0f7a999729480cc9f767eea7dd5b7435e03ea
Naohide Sano <namachapanda2005@gmail.com>
2022-09-19 05:54:38 +0900

    follow ide advices

--- src/main/java/vavi/net/http/HttpServer.java
+++ src/main/java/vavi/net/http/HttpServer.java
@@ -12,12 +12,13 @@ import java.io.OutputStream;
 import java.net.InetAddress;
 import java.net.InetSocketAddress;
 import java.net.Socket;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.logging.Level;
 
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import javax.swing.event.EventListenerList;
 
 import vavi.net.inet.InetServer;
 import vavi.net.inet.SocketHandlerFactory;
@@ -61,85 +62,80 @@ public class HttpServer extends InetServer {
     private InetAddress address;
 
     /** httpRequest */
-    private EventListenerList httpRequestListenerList = new EventListenerList();
+    private List<HttpRequestListener> httpRequestListeners = new ArrayList<>();
 
     /** */
     public void addRequestListener(HttpRequestListener listener) {
-        httpRequestListenerList.add(HttpRequestListener.class, listener);
+        httpRequestListeners.add(listener);
     }
 
     /** */
     public void removeRequestListener(HttpRequestListener listener) {
-        httpRequestListenerList.remove(HttpRequestListener.class, listener);
+        httpRequestListeners.remove(listener);
     }
 
     /** */
     protected void performRequestListener(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        Object[] listeners = httpRequestListenerList.getListenerList();
-        for (int i = listeners.length - 2; i >= 0; i -= 2) {
-            if (listeners[i] == HttpRequestListener.class) {
-                ((HttpRequestListener) listeners[i + 1]).doService(request, response);
-            }
+        for (HttpRequestListener listener : httpRequestListeners) {
+            listener.doService(request, response);
         }
     }
 
     /** 実際のサーバータスク */
-    private SocketHandlerFactory socketHandlerFactory = new SocketHandlerFactory() {
-        public Runnable getSocketHandler(final Socket socket) {
+    private SocketHandlerFactory socketHandlerFactory = socket -> {
 Debug.println(Level.FINE, "<<<< <<<< <<<< <<<< <<<< <<<< <<<< <<<< <<<< ACCEPT: " + socket.getRemoteSocketAddress() + ": " + socket.hashCode());
-            return new Runnable() {
-                public void run() {
-                    InputStream is;
-                    OutputStream os = null;
+        return new Runnable() {
+            public void run() {
+                InputStream is;
+                OutputStream os = null;
 Debug.println(Level.FINE, "==== ONE PROCESS START: " + socket.getRemoteSocketAddress() + ": " + socket.hashCode());
-                    try {
-                        is = socket.getInputStream();
-                        os = socket.getOutputStream();
+                try {
+                    is = socket.getInputStream();
+                    os = socket.getOutputStream();
 
-                        // request
-                        HttpContext requestContext = new HttpContext();
-                        requestContext.setRemoteHost(((InetSocketAddress) socket.getRemoteSocketAddress()).getHostName());
-                        requestContext.setRemotePort(((InetSocketAddress) socket.getRemoteSocketAddress()).getPort());
-                        requestContext.setLocalHost(((InetSocketAddress) socket.getLocalSocketAddress()).getHostName());
-                        requestContext.setLocalPort(((InetSocketAddress) socket.getLocalSocketAddress()).getPort());
-                        HttpUtil.parseRequestHeader(is, requestContext);
+                    // request
+                    HttpContext requestContext = new HttpContext();
+                    requestContext.setRemoteHost(((InetSocketAddress) socket.getRemoteSocketAddress()).getHostName());
+                    requestContext.setRemotePort(((InetSocketAddress) socket.getRemoteSocketAddress()).getPort());
+                    requestContext.setLocalHost(((InetSocketAddress) socket.getLocalSocketAddress()).getHostName());
+                    requestContext.setLocalPort(((InetSocketAddress) socket.getLocalSocketAddress()).getPort());
+                    HttpUtil.parseRequestHeader(is, requestContext);
 //Debug.println("<<<< REQUEST:\n" + StringUtil.paramString(requestContext));
-                        HttpServletRequest request = new HttpServletRequestAdapter(requestContext);
-                        // response
-                        HttpContext responseContext = new HttpContext();
-                        responseContext.setRemoteHost(((InetSocketAddress) socket.getRemoteSocketAddress()).getHostName());
-                        responseContext.setRemotePort(((InetSocketAddress) socket.getRemoteSocketAddress()).getPort());
-                        responseContext.setLocalHost(((InetSocketAddress) socket.getLocalSocketAddress()).getHostName());
-                        responseContext.setLocalPort(((InetSocketAddress) socket.getLocalSocketAddress()).getPort());
-                        responseContext.setProtocol(requestContext.getProtocol()); // TODO check
-                        responseContext.setOutputStream(os);
-                        HttpServletResponse response = new HttpServletResponseAdapter(responseContext);
+                    HttpServletRequest request = new HttpServletRequestAdapter(requestContext);
+                    // response
+                    HttpContext responseContext = new HttpContext();
+                    responseContext.setRemoteHost(((InetSocketAddress) socket.getRemoteSocketAddress()).getHostName());
+                    responseContext.setRemotePort(((InetSocketAddress) socket.getRemoteSocketAddress()).getPort());
+                    responseContext.setLocalHost(((InetSocketAddress) socket.getLocalSocketAddress()).getHostName());
+                    responseContext.setLocalPort(((InetSocketAddress) socket.getLocalSocketAddress()).getPort());
+                    responseContext.setProtocol(requestContext.getProtocol()); // TODO check
+                    responseContext.setOutputStream(os);
+                    HttpServletResponse response = new HttpServletResponseAdapter(responseContext);
 
-                        performRequestListener(request, response);
+                    performRequestListener(request, response);
 
-                        if (!response.isCommitted()) {
-                            response.flushBuffer();
-                        }
+                    if (!response.isCommitted()) {
+                        response.flushBuffer();
+                    }
 //                  } catch (ServletException e) {
 //                      HttpUtil.printErrorResponse(os, e);
 //                  } catch (IOException e) {
 //                      HttpUtil.printErrorResponse(os, e);
-                    } catch (Exception e) {
+                } catch (Exception e) {
 Debug.println(Level.FINE, "==== ERROR RESPONSE: " + e);
-                        // TODO response reset
-                        HttpUtil.printErrorResponse(os, e);
-                    } finally {
-                        try {
-                            os.flush();
+                    // TODO response reset
+                    HttpUtil.printErrorResponse(os, e);
+                } finally {
+                    try {
+                        os.flush();
 Debug.println(Level.FINE, "==== ONE PROCESS DONE: " + socket.getRemoteSocketAddress());
-                            socket.close();
-                        } catch (IOException e) {
-                            e.printStackTrace(System.err);
-                        }
+                        socket.close();
+                    } catch (IOException e) {
+                        e.printStackTrace(System.err);
                     }
                 }
-            };
-        }
+            }
+        };
     };
 
     /** */
